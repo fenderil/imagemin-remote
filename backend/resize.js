d972e0e8e56b3dfe7b53f6f171d2ba95f5168deb
tupitskiy ilya <fenderil@yandex.ru>
2023-03-06 00:24:59 +0300

    resize png buffer to sharp

--- backend/resize.js
+++ backend/resize.js
@@ -1,51 +1,44 @@
 import fs from 'node:fs'
 
-import resizePngBuffer from 'resize-png-buffer'
+import sharp from 'sharp'
 
 const parseResizeConfig = (value) =>
     value.split('\n').reduce((memo, row) => {
         row = row.trim()
         
         if (row) {
-            memo.push(row.split(' ').map((str) => str.trim()).slice(0, 2))
+            const [size, suffix = ''] = row.split(' ').map((str) => str.trim())
+            memo.push({ size, suffix })
         }
         
         return memo
     }, [])
 
-const resizePromise = (resizeCurrentFile, [size, suffix], fileMeta) => new Promise((resolve, reject) => {
-    resizeCurrentFile([parseFloat(size), parseFloat(size)], (err, buffer) => {
-        if (err) {
-            return reject(err)
-        }
-
-        const nextName = fileMeta.originalname.replace('.png', `${suffix}.png`)
-        const nextPath = fileMeta.path.replace(fileMeta.filename, nextName)
-
-        fs.writeFileSync(nextPath, buffer)
-
-        const nextMeta = {
-            ...fileMeta,
-            filename: nextName,
-            originalname: nextName,
-            path: nextPath
-        }
-
-        return resolve(nextMeta)
-    })
-})
-
 export const resize = async (resizeConfig, fileMetas, cb) => {
     const resizedFileMetas = []
-    const sizes = parseResizeConfig(resizeConfig)
+    const scales = parseResizeConfig(resizeConfig)
 
-    if (sizes.length) {
+    if (scales.length) {
         for (let fileMeta of fileMetas) {
-            const resizeCurrentFile = resizePngBuffer(fs.readFileSync(fileMeta.path))
+            for (let scale of scales) {
+                const buffer = await sharp(fileMeta.path)
+                    .resize(parseFloat(scale.size), parseFloat(scale.size))
+                    .png()
+                    .toBuffer()
+                    
+                const nextName = fileMeta.originalname.replace('.png', `${scale.suffix}.png`)
+                const nextPath = fileMeta.path.replace(fileMeta.filename, nextName)
 
-            for (let size of sizes) {
-                const resizedFileMeta = await resizePromise(resizeCurrentFile, size, fileMeta)
-                resizedFileMetas.push(resizedFileMeta)
+                fs.writeFileSync(nextPath, buffer)
+
+                const nextMeta = {
+                    ...fileMeta,
+                    filename: nextName,
+                    originalname: nextName,
+                    path: nextPath
+                }
+
+                resizedFileMetas.push(nextMeta)
                 cb(resizedFileMetas)
             }
         }
